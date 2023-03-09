1d394f1015993bf9b095a307fb385e345c651a15
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-03-09 10:04:02 +0100

    send request to api

--- whatheure.js
+++ whatheure.js
@@ -1,19 +1,14 @@
+import dataJson from "./keys.json" assert { type: "json" };
+
 const form = document.getElementById('timezoneForm');
 const tableBody = document.getElementById('tableBody');
 
-form.addEventListener('submit', function(event) {
+// Get the access key from the JSON data
+const access_key = dataJson.access_key;
+
+document.getElementById('timezoneForm').addEventListener('submit', function(event) {
     event.preventDefault(); // prevent form submission
-
     const locationInput = document.querySelector('#locationInput').value;
-
-    var access_key = fetch('keys.json')
-        .then(response => response.json())
-        .then(data => {
-            return data.key;
-        })
-        .catch(error => {
-            console.error('Error loading configuration file:', error);
-        });
 
     const params = new URLSearchParams({
         access_key: access_key,
@@ -23,17 +18,18 @@ form.addEventListener('submit', function(event) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.positionstack.com/v1/forward?' + params.toString());
     xhr.onload = function() {
+        console.log(xhr.status);
         if (xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
-
+            console.log(response);
             // create new table row and cells
             const newRow = tableBody.insertRow();
             const locationCell = newRow.insertCell();
             const timezoneCell = newRow.insertCell();
 
             // populate cells with data
-            locationCell.textContent = response.data.label;
-            timezoneCell.textContent = response.data.timezone_module.offset_sec;
+            locationCell.textContent = response.label;
+            timezoneCell.textContent = response.timezone_module.offset_sec;
         } else {
             console.log('Request failed. Returned status of ' + xhr.status);
         }
