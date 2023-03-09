9e79325f9985221f3591373e3791d211d0149a29
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-03-09 10:38:55 +0100

    get a response from the api

--- whatheure.js
+++ whatheure.js
@@ -1,4 +1,4 @@
-import dataJson from "./keys.json" assert { type: "json" };
+import dataJson from "./keys.json" assert {type: "json"};
 
 const form = document.getElementById('timezoneForm');
 const tableBody = document.getElementById('tableBody');
@@ -6,30 +6,26 @@ const tableBody = document.getElementById('tableBody');
 // Get the access key from the JSON data
 const access_key = dataJson.access_key;
 
-document.getElementById('timezoneForm').addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault(); // prevent form submission
     const locationInput = document.querySelector('#locationInput').value;
 
     const params = new URLSearchParams({
         access_key: access_key,
-        query: locationInput
+        query: locationInput,
+        timezone_module: 1
     });
 
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.positionstack.com/v1/forward?' + params.toString());
-    xhr.onload = function() {
-        console.log(xhr.status);
+    xhr.onload = function () {
         if (xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
-            console.log(response);
-            // create new table row and cells
-            const newRow = tableBody.insertRow();
-            const locationCell = newRow.insertCell();
-            const timezoneCell = newRow.insertCell();
+            console.log(response.data[0].label);
+            console.log(response.data[0].timezone_module.offset_sec);
+            const now = new Date();
+            console.log
 
-            // populate cells with data
-            locationCell.textContent = response.label;
-            timezoneCell.textContent = response.timezone_module.offset_sec;
         } else {
             console.log('Request failed. Returned status of ' + xhr.status);
         }
