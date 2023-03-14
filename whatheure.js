b24b95dd1ff4e34a8f36782237d4c7b5667d9e9f
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-03-14 09:00:09 +0100

    working

--- whatheure.js
+++ whatheure.js
@@ -21,10 +21,25 @@ form.addEventListener('submit', function (event) {
     xhr.onload = function () {
         if (xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
+            var location = response.data[0].label;
+            var offset = response.data[0].timezone_module.offset_sec;
             console.log(response.data[0].label);
             console.log(response.data[0].timezone_module.offset_sec);
             const now = new Date();
-            console.log
+            var time_in_location = new Date(now - offset);
+            console.log(time_in_location.toLocaleString());
+
+            // Create a new row in the table for each location in the table body timezoneTableBody
+            let tableBody = document.getElementById('timezoneTableBody');
+            const newRow = tableBody.insertRow();
+            const locationCell = newRow.insertCell();
+            const timeCell = newRow.insertCell();
+            locationCell.innerHTML = location;
+            timeCell.innerHTML = time_in_location.toLocaleString();
+            setInterval(function() {
+                time_in_location = new Date(Date.now() - offset);
+                timeCell.innerHTML = time_in_location.toLocaleString();
+            }, 1000);
 
         } else {
             console.log('Request failed. Returned status of ' + xhr.status);
