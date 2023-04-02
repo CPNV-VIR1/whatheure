444c1bf8a99a6ac89fb8efb5d17ddba9760a141c
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2023-04-02 15:38:39 +0200

    Active button when switching lang

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -10,18 +10,35 @@ document.addEventListener('DOMContentLoaded', function () {
     var timeHead = document.getElementById('timeHead')
     var currentLang = navigator.language || navigator.userLanguage
 
+    if (currentLang.startsWith('en')) {
+        btnEn.classList.add('bg-indigo-300');
+    } else if (currentLang.startsWith('fr')) {
+        btnFr.classList.add('bg-indigo-300');
+    } else if (currentLang.startsWith('de')) {
+        btnDe.classList.add('bg-indigo-300');
+    }
+
     translatePage(currentLang.substring(0, 2))
 
     btnEn.addEventListener('click', function () {
         translatePage('en')
+        btnEn.classList.add('bg-indigo-300')
+        btnDe.classList.remove('bg-indigo-300')
+        btnFr.classList.remove('bg-indigo-300')
     })
 
     btnFr.addEventListener('click', function () {
         translatePage('fr')
+        btnEn.classList.remove('bg-indigo-300')
+        btnDe.classList.remove('bg-indigo-300')
+        btnFr.classList.add('bg-indigo-300')
     })
 
     btnDe.addEventListener('click', function () {
         translatePage('de')
+        btnEn.classList.remove('bg-indigo-300')
+        btnDe.classList.add('bg-indigo-300')
+        btnFr.classList.remove('bg-indigo-300')
     })
 
     function translatePage(lang) {
