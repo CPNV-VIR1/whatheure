a3d463a2b1545e45c008854438530cab9d79e15d
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-06-09 10:17:14 +0200

    js loaded

--- serverExpress.js
+++ serverExpress.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+app.use('/assets', express.static('assets'));
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     console.log('Hello World!');
