cc79e5a651070214f870ce53faf45a297a2081bb
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-06-09 10:14:37 +0200

    display view

--- serverExpress.js
+++ serverExpress.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     console.log('Hello World!');
-    res.send('Hello World!');
+    res.status(200);
+    res.render('index');
 });
 const port = 8888;
  app.listen(port);
