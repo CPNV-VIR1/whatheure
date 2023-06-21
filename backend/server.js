7a96b688839b9491f838ade2690ba21a2d1493f0
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-06-21 12:53:47 +0200

    add mariaDB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mariaDB = require('./mariadb.js');
 const path = require('path');
 const apiRouter = require('./routes/api');
 const dotenv = require('dotenv');
@@ -13,8 +14,9 @@ app.use('/api', apiRouter);
 app.set('view engine', 'ejs');
 app.set('views', path.join('../frontend', 'public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const locations = await mariaDB.getTimes();
+    console.log(locations);
 });
 
 app.listen(8888);
