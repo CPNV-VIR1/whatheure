cd7f86f92d50de9e95a05073e979faea51e95afb
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-06-21 16:58:28 +0200

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,11 @@
 import './public/js/defaulttime.js'
 import './public/js/lang.js'
 import './public/js/whatheure.js'
+
+document.querySelector('#timezoneTable').innerHTML = `
+    <tr class="w-full">
+        <td id="locationText">gggg</td>
+        <td id="timeText">ddddd</td>
+    </tr>
+`
+
